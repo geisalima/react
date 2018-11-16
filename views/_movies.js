bedfcc85bf686d5f44359f5f061a9c9ae91ecc2a
Geisa Lima <geisa@opovodigital.com>
2018-11-16 18:51:04 -0300

    subindo arquivos

--- views/_movies.js
+++ views/_movies.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Picker, Button, Text } from 'react-native';
-import { SearchBar, List, ListItem } from 'react-native-elements';
+import { View, Picker, Button, ActivityIndicator, Alert } from 'react-native';
+import { SearchBar, ListItem } from 'react-native-elements';
 import axios from 'axios';
 
 export class Movies extends Component {
@@ -8,85 +8,100 @@ export class Movies extends Component {
     super(props);
     this.state = {
       data: [],
-      term: '',
-      PickerValue: ''
+      PickerValue: '',
+      isLoading: true
     };
   }
 
+  componentDidMount() {
+    return fetch('http://fipeapi.appspot.com/api/1/carros/veiculos/marcas.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson
+        }, function () {
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  // GetPickerSelectedItemValue = () => {
+
+  //   Alert.alert(this.state.PickerValue);
+
+  // }
+
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <View>
+
         <Picker
           selectedValue={this.state.PickerValue}
-          style={{ height: 50, width: 300 }}
-          onValueChange={(itemValue, indexValue) => this.setState({ PickerValue: itemValue })}
-        >
+          onValueChange={(itemValue, indexValue) => this.setState({ PickerValue: itemValue })} >
+
           <Picker.Item label="Selecione o tipo de veiculo" value="" />
-          <Picker.Item label="Carros" value="carros" />
-          <Picker.Item label="Fiat" value="21" />
+          {this.state.dataSource.map((item, key) => (
+            <Picker.Item label={item.fipe_name} value={item.id} key={key} />)
+          )}
         </Picker>
 
-        <Button title="Buscar" onPress={() => this.getFilmes()} />
+        <Button title="Buscar" onPress={() => this.getVeiculos()} />
 
-          {this.state.data.map((item, i) => (
-            <ListItem key={i} title={item.name} />
-          ))}
+        {this.state.data.map((item, i) => (
+          <ListItem key={i} title={item.name} />
+        ))}
 
-        {/* <SearchBar
-          lightTheme
-          onChangeText={term => this.setState({ term })}
-          onClearText={() => this.setState({ term: '' })}
-          placeholder="Faça sua busca aqui..."
-          autoCapitalize="none"
-          autoCorrect={false}
-          onEndEditing={() => this.getFilmes()}
-        />
-        <List containerStyle={{ flex: 1 }}>
-          {this.state.data.map((item, i) => (
-            <ListItem key={i} title={item.nome} />
-          ))}
-        </List> */}
-
-        {/* <ScrollView
-          pagingEnabled={true}
-          onScrollEndDrag={() => {
-            this.state.data.length < this.state.totalResults ? this.getFilmes(false) : null;
-          }}
-        >
-          <List containerStyle={{ flex: 1 }}>
-            {this.state.data.map((item, i) => (
-              <ListItem roundAvatar avatar={{ uri: item.Poster }} key={i} title={item.Title} />
-            ))}
-          </List>
-        </ScrollView> */}
       </View>
+
+      // <View>
+      //   <Picker
+      //     selectedValue={this.state.PickerValue}
+      //     style={{ height: 50, width: 300 }}
+      //     onValueChange={(itemValue, indexValue) => this.setState({ PickerValue: itemValue })}
+      //   >
+      //     <Picker.Item label="Selecione o tipo de veiculo" value="" />
+      //     <Picker.Item label="Carros" value="carros" />
+      //     <Picker.Item label="Fiat" value="21" />
+      //   </Picker>
+
+      //   <Button title="Buscar" onPress={() => this.getVeiculos()} />
+
+      //   {this.state.data.map((item, i) => (
+      //     <ListItem key={i} title={item.name} />
+      //   ))}
+      // </View>
     );
   }
 
-  getFilmes = (clear = true) => {
+  getVeiculos = (clear = true) => {
     var data = this.state.PickerValue;
 
     if (data == "") {
       alert("Escolha uma opção de veiculo");
     } else {
-
-      // let page = this.state.data.length == 0 ? 1 : Math.ceil(this.state.data.length / 10) + 1;
-
-      this.state.PickerValue &&
-        this.state.PickerValue.trim().length > 0 &&
-        axios
-          .get(`http://fipeapi.appspot.com/api/1/carros/veiculos/${this.state.PickerValue}.json`)
-          // .get(`https://parallelum.com.br/fipe/api/v1/${this.state.PickerValue}/marcas`)
-          // http://fipeapi.appspot.com/api/1/[tipo]/[acao]/[parametros].json
-          // http://www.omdbapi.com/?apikey=363b2c11&s=${this.state.term}&page=${page}
-          .then(resp => {
-            clear
-              ? this.setState({ data: resp.data, totalResults: resp.data.totalResults })
-              : this.setState({ data: [...this.state.data, ...resp.data], totalResults: resp.data.totalResults });
-          })
-          .catch(err => {
-            console.log(err);
-          });
+      
+      axios
+        .get(`http://fipeapi.appspot.com/api/1/carros/veiculos/${this.state.PickerValue}.json`)
+        .then(resp => {
+          clear
+            ? this.setState({ data: resp.data, totalResults: resp.data.totalResults })
+            : this.setState({ data: [...this.state.data, ...resp.data], totalResults: resp.data.totalResults });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   };
 }
