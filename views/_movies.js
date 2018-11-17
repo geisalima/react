0e661b70f573253daf421edaefbd64bc55b085fe
Geisa Lima <geisa@opovodigital.com>
2018-11-17 14:03:33 -0300

    populando novos select

--- views/_movies.js
+++ views/_movies.js
@@ -9,8 +9,50 @@ export class Movies extends Component {
     this.state = {
       data: [],
       PickerValue: '',
-      isLoading: true
+      isLoading: true,
+      throttlemode: '',
+      throttlemode2: '',
+      throttlemode3: '',
     };
+  }
+
+  onPickerValueChange = (value, index) => {
+    this.setState(
+      {
+        "throttlemode": value
+      },
+      () => {
+        // here is our callback that will be fired after state change.
+        // Alert.alert();
+        this.getVeiculos();
+      }
+    );
+  }
+
+  onPickerValueChange2 = (value, index) => {
+    this.setState(
+      {
+        "throttlemode2": value
+      },
+      () => {
+        // here is our callback that will be fired after state change.
+        // Alert.alert();
+        this.getModelos();
+      }
+    );
+  }
+
+  onPickerValueChange3 = (value, index) => {
+    this.setState(
+      {
+        "throttlemode3": value
+      },
+      () => {
+        // here is our callback that will be fired after state change.
+        // Alert.alert();
+        this.getAnos();
+      }
+    );
   }
 
   componentDidMount() {
@@ -42,14 +84,15 @@ export class Movies extends Component {
       );
     }
 
-    const { navigate } = this.props.navigation;
-
     return (
       <View>
 
         <Picker
-          selectedValue={this.state.PickerValue}
-          onValueChange={(itemValue, indexValue) => this.setState({ PickerValue: itemValue })} >
+          // selectedValue={this.state.PickerValue}
+          // onValueChange={(itemValue, indexValue) => this.setState({ PickerValue: itemValue })} >
+          selectedValue={this.state.throttlemode}
+          onValueChange={this.onPickerValueChange}
+        >
 
           <Picker.Item label="Selecione a marca do veiculo" value="" />
           {this.state.dataSource.map((item, key) => (
@@ -57,27 +100,45 @@ export class Movies extends Component {
           )}
         </Picker>
 
-        <Button title="Buscar" onPress={() => this.getVeiculos()} />
-
-        <ScrollView>
+        <Picker
+          selectedValue={this.state.throttlemode2}
+          onValueChange={this.onPickerValueChange2}
+        >
           {this.state.data.map((item, i) => (
-            <ListItem key={i} title={item.name} onPress={() => this.onDetalhes(item) } />
+            <Picker.Item key={i} label={item.name} value={item.id} />
           ))}
-        </ScrollView>
+        </Picker>
+        
+        <Picker
+          selectedValue={this.state.throttlemode3}
+          onValueChange={this.onPickerValueChange3}
+        >
+          {this.state.data.map((item, i) => (
+            <Picker.Item key={i} label={item.name} value={item.id} />
+          ))}
+        </Picker>
+
+        {/* <Button title="Buscar" onPress={() => this.getVeiculos()} /> */}
+
+        {/* <ScrollView>
+          {this.state.data.map((item, i) => (
+            <ListItem key={i} title={item.name} onPress={() => this.onDetalhes(item)} />
+          ))}
+        </ScrollView> */}
 
       </View>
     );
   }
 
   getVeiculos = (clear = true) => {
-    var data = this.state.PickerValue;
+    var data = this.state.throttlemode;
 
     if (data == "") {
       alert("Escolha uma opção de marca");
     } else {
 
       axios
-        .get(`http://fipeapi.appspot.com/api/1/carros/veiculos/${this.state.PickerValue}.json`)
+        .get(`http://fipeapi.appspot.com/api/1/carros/veiculos/${this.state.throttlemode}.json`)
         .then(resp => {
           clear
             ? this.setState({ data: resp.data, totalResults: resp.data.totalResults })
@@ -88,4 +149,43 @@ export class Movies extends Component {
         });
     }
   };
+
+  getModelos = (clear = true) => {
+    var data = this.state.throttlemode2;
+    if (data == "") {
+      alert("Escolha uma opção de marca");
+    } else {
+
+      axios
+        .get(`http://fipeapi.appspot.com/api/1/carros/veiculo/${this.state.throttlemode}/${this.state.throttlemode2}.json`)
+        .then(resp => {
+          clear
+            ? this.setState({ data: resp.data, totalResults: resp.data.totalResults })
+            : this.setState({ data: [...this.state.data, ...resp.data], totalResults: resp.data.totalResults });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+
+  getAnos = (clear = true) => {
+    var data = this.state.throttlemode3;
+    if (data == "") {
+      alert("Escolha uma opção de marca");
+    } else {
+
+      axios
+        .get(`http://fipeapi.appspot.com/api/1/carros/veiculo/${this.state.throttlemode}/${this.state.throttlemode2}/${this.state.throttlemode3}.json`)
+        .then(resp => {
+          clear
+            ? this.setState({ data: resp.data, totalResults: resp.data.totalResults })
+            : this.setState({ data: [...this.state.data, ...resp.data], totalResults: resp.data.totalResults });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+
 }
